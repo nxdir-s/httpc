62e4d923777db28c1e6b1944492b78ddedddd9c0
nxdir-s <snadir177@gmail.com>
2026-05-12 21:43:01 -0400

    feat: add Send method

--- client_test.go
+++ client_test.go
@@ -22,11 +22,153 @@ import (
 //go:embed testdata/response.json
 var testData []byte
 
+//go:embed testdata/error.json
+var testError []byte
+
 const (
 	TestEndpoint  string = "/resource"
 	TestResponse  string = "{}"
 	TestRateLimit int    = 60
 )
+
+func TestSend(t *testing.T) {
+	cases := []struct {
+		endpoint     string
+		method       string
+		headers      map[string]string
+		body         io.Reader
+		handler      func(http.ResponseWriter, *http.Request)
+		opts         []ClientOpt
+		in           []byte
+		expectedCode int
+		expectedErr  error
+	}{
+		{
+			endpoint: TestEndpoint,
+			method:   http.MethodGet,
+			headers:  nil,
+			body:     nil,
+			handler: func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write(testData)
+			},
+			opts: []ClientOpt{
+				WithDefaultHeaders(map[string]string{
+					"Cache-Control": "no-cache",
+				}),
+			},
+			in:           testData,
+			expectedCode: http.StatusOK,
+			expectedErr:  nil,
+		},
+		{
+			endpoint: TestEndpoint,
+			method:   http.MethodPost,
+			headers:  nil,
+			body:     bytes.NewReader([]byte(`{}`)),
+			handler: func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write(testData)
+			},
+			opts:         []ClientOpt{},
+			in:           testData,
+			expectedCode: http.StatusOK,
+			expectedErr:  nil,
+		},
+		{
+			endpoint: TestEndpoint,
+			method:   http.MethodPut,
+			headers:  nil,
+			body:     bytes.NewReader([]byte(`{}`)),
+			handler: func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write(testData)
+			},
+			opts:         []ClientOpt{},
+			in:           testData,
+			expectedCode: http.StatusOK,
+			expectedErr:  nil,
+		},
+		{
+			endpoint: TestEndpoint,
+			method:   http.MethodDelete,
+			headers:  nil,
+			body:     bytes.NewReader([]byte(`{}`)),
+			handler: func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write(testData)
+			},
+			opts:         []ClientOpt{},
+			in:           testData,
+			expectedCode: http.StatusOK,
+			expectedErr:  nil,
+		},
+		{
+			endpoint: TestEndpoint,
+			method:   http.MethodPatch,
+			headers:  nil,
+			body:     bytes.NewReader([]byte(`{}`)),
+			handler: func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write(testData)
+			},
+			opts:         []ClientOpt{},
+			in:           testData,
+			expectedCode: http.StatusOK,
+			expectedErr:  nil,
+		},
+	}
+
+	ctx, cancel := context.WithCancel(t.Context())
+	defer cancel()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	for i, tt := range cases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			mux := http.NewServeMux()
+			mux.HandleFunc("/", tt.handler)
+
+			ts := httptest.NewServer(mux)
+
+			cfg := &Config{
+				BaseUrl:   ts.URL,
+				TlsConfig: &tls.Config{},
+			}
+
+			tt.opts = append(tt.opts, WithCustomClient(ts.Client()))
+
+			client, err := NewClient(ctx, cfg, tt.opts...)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			type Response struct {
+				Status  string `json:"status,omitempty"`
+				Message string `json:"message,omitempty"`
+				Data    []struct {
+					Key string `json:"key"`
+				} `json:"data,omitempty"`
+			}
+
+			var in Response
+			if err := json.NewDecoder(bytes.NewReader(tt.in)).Decode(&in); err != nil {
+				t.Fatal(err)
+			}
+
+			var out Response
+			resp, err := client.Send(ctx, tt.method, tt.endpoint, tt.body, tt.headers, &out)
+
+			assert.Equal(t, tt.expectedErr, err)
+			assert.Equal(t, tt.expectedCode, resp.StatusCode)
+
+			assert.True(t, reflect.DeepEqual(in, out))
+
+			ts.Close()
+		})
+	}
+}
 
 func TestGet(t *testing.T) {
 	cases := []struct {
